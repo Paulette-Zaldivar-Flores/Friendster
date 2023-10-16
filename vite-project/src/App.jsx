642f2e2ccabf121e74e3fe6beac6da246aa36bf8
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-10-16 20:13:46 +0900

    chore: added navbar html

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,15 @@
-
+import React from "react";
 import './App.css'
+import  Navbar  from "./components/Navbar";
+
 
 function App() {
   return (
-    <>
       <div>
+        <Navbar/>
         <h1>Vite + React</h1>
       </div>
-    </>
-  )
+  );
 }
 
 export default App
---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../assets/stylesheets/components/_navbar.scss';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/services">Services</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
