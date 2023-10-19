380d9fa68c6571474f760beaa9541d52f2ffa424
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-10-19 21:27:12 +0900

    added bootstrap and responsive navbar with styles

--- friendster-frontend/vite-project/src/components/Navbar.jsx
+++ friendster-frontend/vite-project/src/components/Navbar.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../assets/Friendster.png'
 
-
-
-function Navbar() {
+function Navigation() {
   return (
-    <nav className='navbar'>
-      <div className="logo">
-        <img src={Logo} alt="Logo"  className='logo'/>
-      </div>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/services">Services</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <Navbar expand="lg" className="navbar">
+      <Container>
+      <Navbar.Brand href="#home">
+            <img
+              src={ Logo }
+              width="60"
+              height="60"
+              className="logo d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto d-flex center-links">
+            <Nav.Link href="#home" className = "ms-3">Home</Nav.Link>
+            <Nav.Link href="#link" className = "ms-3">Link</Nav.Link>
+            <Button variant="success" className = "ms-3">Log Out</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default Navigation;
