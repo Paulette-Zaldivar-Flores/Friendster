b6626f34df7ea87f2b56e4b153bc020003348ec8
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-10-26 20:02:39 +0900

    test: footer branch

--- friendster-frontend/vite-project/src/App.jsx
+++ friendster-frontend/vite-project/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import './App.css'
 import  Navbar  from "./components/Navbar"
+import Footer from "./components/Footer"
 
 
 function App() {
   return (
-      <div>
+      <div className = "App">
         <Navbar/>
         <h1>Vite + React</h1>
+        <Footer/>
       </div>
   );
 }
