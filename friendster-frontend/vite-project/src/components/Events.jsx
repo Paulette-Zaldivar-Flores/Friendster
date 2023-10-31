9bdce7bbd43a592d5118b6b76affd2f838ff7194
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-10-31 20:10:47 +0900

    refactor: added responsive events cards

---
+++ friendster-frontend/vite-project/src/components/Events.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+const Events = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3 mb-4">
+          <div className="card event-card">
+            <div className="card-body">
+              <h5 className="card-title">Event 1</h5>
+              <p className="card-text">Description of Event 1</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3 mb-4">
+          <div className="card event-card">
+            <div className="card-body">
+              <h5 className="card-title">Event 2</h5>
+              <p className="card-text">Description of Event 2</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3 mb-4">
+          <div className="card event-card">
+            <div className="card-body">
+              <h5 className="card-title">Event 3</h5>
+              <p className="card-text">Description of Event 3</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3 mb-4">
+          <div className="card event-card">
+            <div className="card-body">
+              <h5 className="card-title">Event 4</h5>
+              <p className="card-text">Description of Event 4</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-3 mb-4">
+          <div className="card event-card">
+            <div className="card-body">
+              <h5 className="card-title">Event 5</h5>
+              <p className="card-text">Description of Event 5</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3 mb-4">
+          <div className="card event-card">
+            <div className="card-body">
+              <h5 className="card-title">Event 6</h5>
+              <p className="card-text">Description of Event 6</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3 mb-4">
+          <div className="card event-card">
+            <div className="card-body">
+              <h5 className="card-title">Event 7</h5>
+              <p className="card-text">Description of Event 7</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3 mb-4">
+          <div className="card event-card">
+            <div className="card-body">
+              <h5 className="card-title">Event 8</h5>
+              <p className="card-text">Description of Event 8</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Events;
--- friendster-frontend/vite-project/src/pages/Home.jsx
+++ friendster-frontend/vite-project/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../components/Banner'
+import Events from '../components/Events'
 
 function Home() {
   return (
     <div className = "home">
       <Banner/>
+      <Events/>
     </div>
   );
 }
