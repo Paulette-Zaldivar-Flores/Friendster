ccb686681f7407ad62880ee202bcf93428ae1291
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-11-01 21:21:31 +0900

    style: styled text in events cards

--- friendster-frontend/vite-project/src/components/Events.jsx
+++ friendster-frontend/vite-project/src/components/Events.jsx
@@ -1,39 +1,48 @@
 import React from 'react';
-import Event from './../assets/event-image.jpg'
+
 
 const Events = () => {
   return (
     <div className="container">
+      <h3>Events near you...</h3>
       <div className="row">
         <div className="col-md-3 mb-4">
           <div className="card event-card">
-            <div className="card-body">
+            <div className="card-body d-flex flex-column">
+            <div className="mt-auto">
               <h5 className="card-title">Event 1</h5>
               <p className="card-text">Description of Event 1</p>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-md-3 mb-4">
           <div className="card event-card">
-            <div className="card-body">
+            <div className="card-body d-flex flex-column">
+            <div className="mt-auto">
               <h5 className="card-title">Event 2</h5>
               <p className="card-text">Description of Event 2</p>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-md-3 mb-4">
           <div className="card event-card">
-            <div className="card-body">
+            <div className="card-body d-flex flex-column">
+            <div className="mt-auto">
               <h5 className="card-title">Event 3</h5>
               <p className="card-text">Description of Event 3</p>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-md-3 mb-4">
           <div className="card event-card">
-            <div className="card-body">
+            <div className="card-body d-flex flex-column">
+            <div className="mt-auto">
               <h5 className="card-title">Event 4</h5>
               <p className="card-text">Description of Event 4</p>
+              </div>
             </div>
           </div>
         </div>
@@ -42,33 +51,41 @@ const Events = () => {
       <div className="row">
         <div className="col-md-3 mb-4">
           <div className="card event-card">
-            <div className="card-body">
+            <div className="card-body d-flex flex-column">
+            <div className="mt-auto">
               <h5 className="card-title">Event 5</h5>
               <p className="card-text">Description of Event 5</p>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-md-3 mb-4">
           <div className="card event-card">
-            <div className="card-body">
+            <div className="card-body d-flex flex-column">
+            <div className="mt-auto">
               <h5 className="card-title">Event 6</h5>
               <p className="card-text">Description of Event 6</p>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-md-3 mb-4">
           <div className="card event-card">
-            <div className="card-body">
+            <div className="card-body d-flex flex-column">
+            <div className="mt-auto">
               <h5 className="card-title">Event 7</h5>
               <p className="card-text">Description of Event 7</p>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-md-3 mb-4">
           <div className="card event-card">
-            <div className="card-body">
+            <div className="card-body d-flex flex-column">
+            <div className="mt-auto">
               <h5 className="card-title">Event 8</h5>
               <p className="card-text">Description of Event 8</p>
+              </div>
             </div>
           </div>
         </div>
