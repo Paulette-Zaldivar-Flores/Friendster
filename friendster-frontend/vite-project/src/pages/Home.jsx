e09341737833bb1f6c2248173d35bb5757241b91
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-11-06 18:10:51 +0900

    refactor: updated home.jsx syntax and added routes.jsx to define routes for app

--- friendster-frontend/vite-project/src/pages/Home.jsx
+++ friendster-frontend/vite-project/src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Banner from '../components/Banner'
-import Events from '../components/Events'
+import Banner from '../components/Banner';
+import Events from '../components/Events';
 import EventDetail from '../components/EventDetail';
 
 function Home() {
   return (
-    <div className = "home">
-      <Banner/>
+    <div className="home">
+      <Banner />
       <Routes>
-      <Route exact path="/*" element={<Events />} />
-      <Route exact path="/events/:id" element={<EventDetail />} />
+        <Route index element={<Events />} />
+        <Route path="events/:id" element={<EventDetail />} />
       </Routes>
     </div>
   );
