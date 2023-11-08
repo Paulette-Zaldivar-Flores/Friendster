8d5fd4ae39f118b942b79655f1c013fd7b6e6d1b
James Chang <zhaoren@gmail.com>
2023-11-08 22:05:54 +0900

    the icons are added but just need to change the color.. tere may be a need to label each icon individually.

---
+++ friendster-frontend/vite-project/src/components/Howitworks.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const HowItWorks = () => {
+  return (
+    <div className="flex-container">
+      <h2 className="title">How it Works</h2>
+        <div className="flex-item">
+          </div>
+            <div className="flex-item">
+              </div>
+        <div className="flex-item">
+          <img src="/src/assets/images/icons/signup.png" alt="signup" className="icon" />
+        </div>
+        <div className="flex-item">
+          </div>
+        <div className="flex-item">
+          <img src="/src/assets/images/icons/searchglass.png" alt="search" className="icon" />
+        </div>
+        <div className="flex-item">
+          </div>
+             <div className="flex-item">
+          <img src="/src/assets/images/icons/share.png" alt="share" className="icon" />
+        </div>
+        <div className="flex-item">
+          </div>
+            <div className="flex-item">
+              </div>
+    </div>
+    );
+};
+
+export default HowItWorks;
--- friendster-frontend/vite-project/src/pages/Home.jsx
+++ friendster-frontend/vite-project/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../components/Banner'
 import Events from '../components/Events'
+import HowItWorks from '../components/Howitworks'
 
 function Home() {
   return (
     <div className = "home">
       <Banner/>
+      <HowItWorks/>
       <Events/>
     </div>
   );
