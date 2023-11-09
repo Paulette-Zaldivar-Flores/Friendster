0968db64e62a4e4a4389af3e72e621a9bfe44a64
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-11-09 21:48:20 +0900

    fix: repaired deleted files

--- friendster-frontend/vite-project/src/components/HowitWorks.jsx
+++ friendster-frontend/vite-project/src/components/HowitWorks.jsx
@@ -1,7 +1,35 @@
+import React from 'react';
 
-import React from 'react'
+const HowItWorks = () => {
+  return (
+    <div className="flex-container">
+      <h2 className="title">How it Works</h2>
+        <div className="flex-item">
+          </div>
+            <div className="flex-item">
+              </div>
+        <div className="flex-item">
+        <i className="fa-solid fa-user"></i>
+        <p className ="flexitem-p">Sign Up</p>
+        </div>
+        <div className="flex-item">
+          </div>
+        <div className="flex-item">
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <p className ="flexitem-p">Search</p>
+        </div>
+        <div className="flex-item">
+          </div>
+            <div className="flex-item">
+              <i className="fa-solid fa-paper-plane"></i>
+              <p className ="flexitem-p">Share</p>
+        </div>
+        <div className="flex-item">
+          </div>
+            <div className="flex-item">
+              </div>
+    </div>
+    );
+};
 
-
-
-
-
+export default HowItWorks;
